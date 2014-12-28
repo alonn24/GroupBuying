11447f6da904c1ed107cb66f650c6c04ca4453a7
alonn24 <alonn24@gmail.com>
2014-12-28 08:16:50 +0200

    Add new files

---
+++ GroupBuyingProject/app/js/services/common/AppMessages.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+angular.module('app.common')
+.service('appMessages', function ($location) {
+    var _this = this;
+    this.message = '';
+    this.type = '';
+
+    this.setMessage = function (message) {
+        this.message = message;
+        this.type = 'success';
+    };
+
+    this.setErrorMessage = function (message) {
+        this.message = message;
+        this.type = 'error';
+    };
+
+    this.clear = function () {
+        this.message = '';
+        this.type = '';
+    };
+});
---
+++ GroupBuyingProject/app/js/services/common/UrlDispacher.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+angular.module('app.common')
+.service('urlDispacher', function ($location) {
+    var _this = this;
+
+    this.navigateToLogIn = function () {
+        $location.path('LogInPage');
+    };
+
+    this.navigateToProduct = function (productId) {
+        if (productId)
+            $location.path('Product/' + productId);
+        else
+            $location.path('Products');
+    };
+});
